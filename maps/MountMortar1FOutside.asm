b098acf077cbdd895dcdd3621872d12b55bc7548
dekzeh <gpessoa90@hotmail.com>
2021-11-09 17:55:32 -0300

    Mount Mortar 1FOutside remapping and event positions

--- maps/MountMortar1FOutside.asm
+++ maps/MountMortar1FOutside.asm
@@ -27,7 +27,7 @@ MountMortar1FOutside_MapEvents:
 	warp_event 11, 21, MOUNT_MORTAR_1F_INSIDE, 1
 	warp_event 29, 21, MOUNT_MORTAR_1F_INSIDE, 2
 	warp_event 17, 29, MOUNT_MORTAR_B1F, 2
-	warp_event  7, 13, MOUNT_MORTAR_1F_INSIDE, 3
+	warp_event  9, 13, MOUNT_MORTAR_1F_INSIDE, 3
 	warp_event 33, 13, MOUNT_MORTAR_1F_INSIDE, 4
 
 	def_coord_events
@@ -36,5 +36,5 @@ MountMortar1FOutside_MapEvents:
 	bg_event 25, 22, BGEVENT_ITEM, MountMortar1FOutsideHiddenHyperPotion
 
 	def_object_events
-	object_event 13, 15, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MountMortar1FOutsideEther, EVENT_MOUNT_MORTAR_1F_OUTSIDE_ETHER
-	object_event 31, 18, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MountMortar1FOutsideRevive, EVENT_MOUNT_MORTAR_1F_OUTSIDE_REVIVE
+	object_event 20, 16, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MountMortar1FOutsideEther, EVENT_MOUNT_MORTAR_1F_OUTSIDE_ETHER
+	object_event  5, 31, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MountMortar1FOutsideRevive, EVENT_MOUNT_MORTAR_1F_OUTSIDE_REVIVE
