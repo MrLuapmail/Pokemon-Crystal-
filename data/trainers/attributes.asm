fea7ee1a44b7009540838fa24f6f35a24e049b8a
KDLPro <38602758+KDLPro@users.noreply.github.com>
2022-06-10 21:41:10 +0800

    Standardize Switching AI

--- data/trainers/attributes.asm
+++ data/trainers/attributes.asm
@@ -54,7 +54,7 @@ TrainerClassAttributes:
 	db NO_ITEM, NO_ITEM ; items
 	db 15 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Pokemon Prof
 	db NO_ITEM, NO_ITEM ; items
@@ -120,7 +120,7 @@ TrainerClassAttributes:
 	db NO_ITEM, NO_ITEM ; items
 	db 25 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Erika
 	db NO_ITEM, NO_ITEM ; items
@@ -132,25 +132,25 @@ TrainerClassAttributes:
 	db NO_ITEM, NO_ITEM ; items
 	db 4 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Schoolboy
 	db NO_ITEM, NO_ITEM ; items
 	db 8 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Bird Keeper
 	db NO_ITEM, NO_ITEM ; items
 	db 6 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Lass
 	db NO_ITEM, NO_ITEM ; items
 	db 6 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Janine
 	db NO_ITEM, NO_ITEM ; items
@@ -162,49 +162,49 @@ TrainerClassAttributes:
 	db NO_ITEM, NO_ITEM ; items
 	db 12 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Cooltrainerf
 	db NO_ITEM, NO_ITEM ; items
 	db 12 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Beauty
 	db NO_ITEM, NO_ITEM ; items
 	db 22 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Pokemaniac
 	db NO_ITEM, NO_ITEM ; items
 	db 15 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Gruntm
 	db NO_ITEM, NO_ITEM ; items
 	db 10 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Gentleman
 	db NO_ITEM, NO_ITEM ; items
 	db 18 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Skier
 	db NO_ITEM, NO_ITEM ; items
 	db 18 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Teacher
 	db NO_ITEM, NO_ITEM ; items
 	db 18 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Sabrina
 	db NO_ITEM, NO_ITEM ; items
@@ -216,37 +216,37 @@ TrainerClassAttributes:
 	db NO_ITEM, NO_ITEM ; items
 	db 4 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Fisher
 	db NO_ITEM, NO_ITEM ; items
 	db 10 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Swimmerm
 	db NO_ITEM, NO_ITEM ; items
 	db 2 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Swimmerf
 	db NO_ITEM, NO_ITEM ; items
 	db 5 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Sailor
 	db NO_ITEM, NO_ITEM ; items
 	db 10 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Super Nerd
 	db NO_ITEM, NO_ITEM ; items
 	db 8 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Rival2
 	db NO_ITEM, NO_ITEM ; items
@@ -258,19 +258,19 @@ TrainerClassAttributes:
 	db NO_ITEM, NO_ITEM ; items
 	db 8 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Hiker
 	db NO_ITEM, NO_ITEM ; items
 	db 8 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Biker
 	db NO_ITEM, NO_ITEM ; items
 	db 8 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Blaine
 	db NO_ITEM, NO_ITEM ; items
@@ -282,25 +282,25 @@ TrainerClassAttributes:
 	db NO_ITEM, NO_ITEM ; items
 	db 22 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Firebreather
 	db NO_ITEM, NO_ITEM ; items
 	db 12 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Juggler
 	db NO_ITEM, NO_ITEM ; items
 	db 10 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Blackbelt T
 	db NO_ITEM, NO_ITEM ; items
 	db 6 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Executivem
 	db NO_ITEM, NO_ITEM ; items
@@ -312,19 +312,19 @@ TrainerClassAttributes:
 	db NO_ITEM, NO_ITEM ; items
 	db 8 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Picnicker
 	db NO_ITEM, NO_ITEM ; items
 	db 5 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Camper
 	db NO_ITEM, NO_ITEM ; items
 	db 5 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Executivef
 	db NO_ITEM, NO_ITEM ; items
@@ -336,43 +336,43 @@ TrainerClassAttributes:
 	db NO_ITEM, NO_ITEM ; items
 	db 8 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Medium
 	db NO_ITEM, NO_ITEM ; items
 	db 10 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Boarder
 	db NO_ITEM, NO_ITEM ; items
 	db 18 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Pokefanm
 	db NO_ITEM, NO_ITEM ; items
 	db 20 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Kimono Girl
 	db NO_ITEM, NO_ITEM ; items
 	db 18 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Twins
 	db NO_ITEM, NO_ITEM ; items
 	db 5 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Pokefanf
 	db NO_ITEM, NO_ITEM ; items
 	db 20 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Red
 	db NO_ITEM, NO_ITEM ; items
@@ -390,18 +390,18 @@ TrainerClassAttributes:
 	db NO_ITEM, NO_ITEM ; items
 	db 10 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Gruntf
 	db NO_ITEM, NO_ITEM ; items
 	db 10 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Mysticalman
 	db NO_ITEM, NO_ITEM ; items
 	db 25 ; base reward
 	dw AI_BASIC | AI_SMART | AI_AGGRESSIVE | AI_STATUS 
-	dw CONTEXT_USE | SWITCH_SOMETIMES
+	dw CONTEXT_USE | SWITCH_OFTEN
 
 	assert_table_length NUM_TRAINER_CLASSES
