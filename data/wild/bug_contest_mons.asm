4342c4e89d9ee9dc90b4e80e0866459d8d6ab544
Luapmail <93484696+Luapmail@users.noreply.github.com>
2022-07-05 11:10:26 +0200

    Update bug_contest_mons.asm

--- data/wild/bug_contest_mons.asm
+++ data/wild/bug_contest_mons.asm
@@ -1,13 +1,13 @@
 ContestMons:
 	;   %, species,   min, max
-	db 20, CATERPIE,    7, 18
-	db 20, WEEDLE,      7, 18
-	db 10, METAPOD,     9, 18
-	db 10, KAKUNA,      9, 18
-	db  5, BUTTERFREE, 12, 15
-	db  5, BEEDRILL,   12, 15
-	db 10, VENONAT,    10, 16
-	db 10, PARAS,      10, 17
-	db  5, SCYTHER,    13, 14
-	db  5, PINSIR,     13, 14
+	db 20, CATERPIE,   25, 25
+	db 20, WEEDLE,     25, 25
+	db 10, METAPOD,    25, 25
+	db 10, KAKUNA,     25, 25
+	db  5, BUTTERFREE, 25, 25
+	db  5, BEEDRILL,   25, 25
+	db 10, VENONAT,    25, 25
+	db 10, PARAS,      25, 25
+	db  5, SCYTHER,    25, 25
+	db  5, PINSIR,     25, 25
 	db -1, VENOMOTH,   30, 40
