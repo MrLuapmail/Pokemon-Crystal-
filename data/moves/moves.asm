b3bda083bd12a0cc8acd96c7ad362b884c0998d2
Luapmail <93484696+Luapmail@users.noreply.github.com>
2022-07-28 11:42:41 +0200

    Update moves.asm

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -10,7 +10,7 @@ move: MACRO
 	db \7 percent ; effect chance
 ENDM
 
-Moves:
+Moves:F
 ; entries correspond to move ids (see constants/move_constants.asm)
 	table_width MOVE_LENGTH, Moves
 	move POUND,        EFFECT_NORMAL_HIT,         40, NORMAL,       100, 35,   0
@@ -148,7 +148,7 @@ Moves:
 	move AMNESIA,      EFFECT_SP_DEF_UP_2,         0, PSYCHIC_TYPE, 100,  2,   0
 	move KINESIS,      EFFECT_ACCURACY_DOWN,       0, PSYCHIC_TYPE, 100,  3,   0
 	move SOFTBOILED,   EFFECT_HEAL,                0, NORMAL,       100, 10,   0
-	move HI_JUMP_KICK, EFFECT_JUMP_KICK,          110, FIGHTING,    100, 20,   0
+	move HI_JUMP_KICK, EFFECT_JUMP_KICK,          110, FIGHTING,     90, 20,   0
 	move GLARE,        EFFECT_PARALYZE,            0, NORMAL,       100,  3,   0
 	move DREAM_EATER,  EFFECT_DREAM_EATER,       100, PSYCHIC_TYPE, 100, 15,   0
 	move POISON_GAS,   EFFECT_POISON,              0, POISON,       100,  3,   0
